d29afc34460ed9942e432e88ec02fc0a776a1cdc
Itaï <salutevg@gmail.com>
2022-09-13 17:17:29 +0200

    master : gui.init

---
+++ org/pyengdrom/gui/index.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QMessageBox
+class EngdromGUI:
+    @staticmethod
+    def main(argv):
+        app=QApplication.instance()
+        fen=QWidget()
+        fen.resize(250, 150)
+        fen.move(300, 300)
+        fen.setWindowTitle('Engdrom GUI')
+        fen.show()
+        app.exec_()
+if __name__ == '__main__':
+    import sys
+    EngdromGUI.main(sys.argv)
+
