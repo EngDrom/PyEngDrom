b452456a6e268d5de24a8473ad10d1b7fa56b9f7
MrThimote <mrthimote@gmail.com>
2022-09-15 21:59:07 +0200

    master: gui.core: added separtors in menubar

--- org/pyengdrom/gui/core/config.py
+++ org/pyengdrom/gui/core/config.py
@@ -9,18 +9,20 @@ MENU_BAR__TEXT_EDITOR = MenuBar(
         ("File", [
             ("New",         "new", "Ctrl+N"),
             ("Open Folder", "open", "Ctrl+O"),
-            ("Save",        [
-                ("Save", "save", "Ctrl+S"),
-                ("Save as", "save_as", "Ctrl+Shift+S")
-            ]),
+            ( None,         "separator"),
+            ("Save", "save", "Ctrl+S"),
+            ("Save as", "save_as", "Ctrl+Shift+S"),
+            ( None,         "separator"),
             ("Exit",        "exit")
         ]),
         ("Edit", [
             ("Undo", "undo",   "Ctrl+Z"),
             ("Redo", "redo",   "Ctrl+Y"),
+            ( None,  "separator"),
             ("Cut", "cut",     "Ctrl+X"),
             ("Copy", "copy",   "Ctrl+C"),
             ("Paste", "paste", "Ctrl+V"),
+            ( None,  "separator"),
             ("Find", "find",   "Ctrl+F")
         ])
     ]
--- org/pyengdrom/gui/core/menubar.py
+++ org/pyengdrom/gui/core/menubar.py
@@ -13,19 +13,22 @@ class MenuBar:
     def __init__(self, config: MenuBarConfig):
         self.config = config
     def _build(self, config: MenuBarConfig, bar=None):
-        if bar is None: bar = QMenuBar()
+        if bar is None: 
+            bar = QMenuBar()
 
         for string, next, *args in config:
             if not isinstance(next, str) and next is not None:
                 menu = bar.addMenu(string)
                 self._build(next, menu)
-            else:
+            elif string is not None:
                 action = bar.addAction(string)
 
                 if next is not None:
                     action.triggered.connect(getattr(self.element, next))
                 if len(args) >= 1:
                     action.setShortcut(args[0])
+            else:
+                bar.addSeparator()
         
         return bar
         
