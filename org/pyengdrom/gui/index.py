c92aecb3c468aae69c170a2464bad4b7eec1c0fc
Itaï <salutevg@gmail.com>
2022-09-19 20:04:00 +0200

    master: gui.text_editor: added some functionnalities for text editor

--- org/pyengdrom/gui/index.py
+++ org/pyengdrom/gui/index.py
@@ -14,13 +14,13 @@ class MainWindow(QMainWindow):
     def resizeEvent(self, a0) -> None:
         if not hasattr(self, "resizeEvents"): self.resizeEvents = []
         for event in self.resizeEvents: event(a0)
-
 class EngdromGUI:
     def make_text_gui(self):
         pass
 
     def __init__(self, args):
         self.array=[]
+        self.filenames=[None]
         self.model=QFileSystemModel()
         # set home folder as foldername
         self.foldername = args.folder
@@ -51,14 +51,14 @@ class EngdromGUI:
         #self.text = OpenGLEngine(args.folder)
 
         # add padding top of 20%
-        #self.text.setStyleselfSheet(f"padding-top: {int(0.3*self.text.height())}px")
-        
         # self.text.setStyleSheet(tailwind("text-white bg-gray-800"))
         #align text horizontally
         #self.text.setAlignment(Qt.AlignHCenter)
         # add logo of EngDrom to the text
         # add margin of 20%
-        self.text.insertHtml("<center><img src='org/pyengdrom/gui/logo.png' width='50' height='50'><br><br><br></center>")
+        self.text.insertHtml("<center><img src='logo.png' width='50' height='50'><br><br><br></center>")
+        self.text.textChanged.connect(self.textchanged)
+        self.text.cursorPositionChanged.connect(self.cursorchanged)
         self.array.append(self.text)
         self.onglets.addTab(self.text, "Welcome to Engdrom")
         # add status bar
@@ -111,6 +111,9 @@ class EngdromGUI:
         text.setStyleSheet("background-color: #2d2d2d; color: #ffffff")
         self.array.append(text)
         self.onglets.addTab(text, "New file")
+        self.filenames.append(None)
+        self.array[-1].textChanged.connect(self.textchanged)
+        self.array[-1].cursorPositionChanged.connect(self.cursorchanged)
     def open(self):
         # get selected file
         index = self.explorer.selectedIndexes()[0]
@@ -123,8 +126,11 @@ class EngdromGUI:
             text.setStyleSheet("background-color: #2d2d2d; color: #ffffff")
             self.array.append(text)
             self.onglets.addTab(text, filename)
+            self.filenames.append(filename)
             # add file content to text editor
             text.setText(f.read())
+            self.array[-1].textChanged.connect(self.textchanged)
+            self.array[-1].cursorPositionChanged.connect(self.cursorchanged)
             self.status.showMessage("File opened")
     def open_folder(self):
         # open folder
@@ -146,18 +152,25 @@ class EngdromGUI:
     def save(self):
         self.status.showMessage("Save file")
         # check if file is already open
-        self.save_as()
+        current_index=self.onglets.currentIndex()
+        if self.filenames[current_index] is not None:
+            if self.filenames[current_index].endswith("*"):
+                with open(self.filenames[current_index][:-1], 'w') as f:
+                    f.write(self.array[current_index].toPlainText())
+        else:
+            self.save_as()
     def save_as(self):
         self.status.showMessage("Save file as")
         # open file dialog
         filename = QFileDialog.getSaveFileName(self.window, 'Save file', '/home')
         if filename[0] == "": return None
-
+        current_index=self.onglets.currentIndex()
         # save file
         with open(filename[0], 'w') as f:
-            f.write(self.text.toPlainText())
+            f.write(self.array[current_index].toPlainText())
         #update onglet name
         self.onglets.setTabText(self.onglets.currentIndex(), filename[0])
+        self.filenames.append(filename[0])
     def exit(self):
         # create messagebox
         msg = QMessageBox()
@@ -175,39 +188,46 @@ class EngdromGUI:
             self.app.exit()
         else:
             pass
+    # TODO : update with self.array[index]
     def undo(self):
-        self.text.undo()
+        current_index=self.onglets.currentIndex()
+        self.array[current_index].undo()
         self.status.showMessage("Undo")
     def redo(self):
-        self.text.redo()
+        current_index=self.onglets.currentIndex()
+        self.array[current_index].redo()
         self.status.showMessage("Redo")
     def cut(self):
-        self.text.cut()
+        current_index=self.onglets.currentIndex()
+        self.array[current_index].cut()
         self.status.showMessage("Cut")
     def copy(self):
-        self.text.copy()
+        current_index=self.onglets.currentIndex()
+        self.array[current_index].copy()
         self.status.showMessage("Copy")
     def paste(self):
-        self.text.paste()
+        current_index=self.onglets.currentIndex()
+        self.array[current_index].paste()
         self.status.showMessage("Paste")
     def find(self):
+        current_index=self.onglets.currentIndex()
         # create a box in the window to ask for susbtring to search
         text, ok = QInputDialog.getText(self.window, 'Find', 'Enter text:')
         # check if user clicked ok
         if ok:
             self.status.showMessage("Find: " + text)
             # search every occurence of the substring
-            cursor = self.text.textCursor()
+            cursor = self.array[current_index].textCursor()
             cursor.setPosition(0)
-            self.text.setTextCursor(cursor)
+            self.array[current_index].setTextCursor(cursor)
             # color every occurence of the substring
-            while self.text.find(text):
+            while self.array[current_index].find(text):
                 fmt = QTextCharFormat()
-                fmt.setBackground(QBrush(QColor(255, 255, 0)))
-                cursor = self.text.textCursor()
+                fmt.setBackground(QBrush(QColor(0, 0, 150)))
+                cursor = self.array[current_index].textCursor()
                 cursor.mergeCharFormat(fmt)
                 cursor.movePosition(QTextCursor.NextWord)
-                self.text.setTextCursor(cursor)
+                self.array[current_index].setTextCursor(cursor)
         else:
             self.status.showMessage("Find")
     def close_onglet(self,index):
@@ -215,6 +235,20 @@ class EngdromGUI:
         # remove onglet
         self.onglets.removeTab(index)
         self.array.pop(index)
+        self.filenames.pop(index)
+    def textchanged(self):
+        current_index=self.onglets.currentIndex()
+        if self.filenames[current_index] is None:
+            self.onglets.setTabText(current_index, "New file *")
+            return
+        if self.filenames[current_index].endswith("*"):
+            return
+        self.onglets.setTabText(current_index, self.filenames[current_index]+" *")
+        self.filenames[current_index]=self.filenames[current_index]+"*"
+    # TODO : regarder doc
+    def cursorchanged(self):
+        current_index=self.onglets.currentIndex()
+        pass
 if __name__ == '__main__':
     import sys
     app=EngdromGUI(sys.argv)
